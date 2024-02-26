c7a4242d077a88fda4a81065ba191aa3f2522ad0
Jonathan Pearce <j.pearce1@nhs.net>
2024-02-26 17:39:58 +0000

    DR-5967 Tagged as v0.3

--- scripts/workflow/action-infra-stack.sh
+++ scripts/workflow/action-infra-stack.sh
@@ -9,7 +9,6 @@
 # fail on first error
 set -e
 # functions
-source ../uec-dos-management/scripts/workflow/functions/terraform-functions.sh
 
 export ACTION="${ACTION:-""}"
 export STACK="${STACK:-""}"
@@ -17,6 +16,8 @@ export ENVIRONMENT="${ENVIRONMENT:-""}"
 export USE_REMOTE_STATE_STORE="${USE_REMOTE_STATE_STORE:-true}"
 export ACCOUNT_PROJECT="${ACCOUNT_PROJECT:-"dos"}"
 export TF_VAR_repo_name="${REPOSITORY:-"$(basename -s .git "$(git config --get remote.origin.url)")"}"
+
+source ../uec-dos-management/scripts/workflow/functions/terraform-functions.sh
 
 # check exports have been done
 EXPORTS_SET=0
