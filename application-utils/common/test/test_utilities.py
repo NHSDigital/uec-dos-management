154aba7a97dc832d0b5f5e86d8df3913728c3464
Tim Rickwood <tim.rickwood@nhs.net>
2024-02-27 15:46:46 +0000

    DR-452 Expand unit tests

---
+++ application-utils/common/test/test_utilities.py
@@ -0,0 +1,14 @@
+from common import utilities
+
+
+def test_default_salutation():
+    "Test salutation"
+    expected_salutation = "Hello"
+    actual_salutation = utilities.get_salutation("English")
+    assert actual_salutation == expected_salutation
+
+def test_german_salutation():
+    "Test salutation"
+    expected_salutation = "Servus"
+    actual_salutation = utilities.get_salutation("German")
+    assert actual_salutation == expected_salutation
