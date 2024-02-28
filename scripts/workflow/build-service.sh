999e29140e57c31b1d228aefe617be5a8f6b8682
Jonathan Pearce <j.pearce1@nhs.net>
2024-02-28 17:24:26 +0000

    DR-5961 Refactor build and deploy stage

---
+++ scripts/workflow/build-service.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# fail on first error
+set -e
+
+export SERVICE="${SERVICE:-""}"
+echo "Building service: ${SERVICE}"
+
+echo "Copy utility code"
+# copy util code but not the test code
+rsync -av --exclude='test/' ./application-utils/* ./application/"${SERVICE}"
+
+cd ./application/"${SERVICE}"
+
+zip -r deployment.zip .
+
+echo "Tidy up temporary files"
+rm -rf ../../application/"${SERVICE}"/common/
