2e5eb98e045176327085bedce60a8d49ebd73096
Tim Rickwood <tim.rickwood@nhs.net>
2024-03-01 12:57:08 +0000

    DR-596 Add github stack and readme

---
+++ scripts/workflow/README.md
@@ -0,0 +1,12 @@
+# Workflow scripts
+
+The scripts in this directory are checked out as part of the checkout-repository action
+(see .github/actions/checkout-repository/action.yaml)
+
+Which version of those scripts is checked out is determined by the management-tag input parameter
+That parameter will be set by default to the latest stable release
+
+If you are changing any of the scripts in this directory or below you are advised to
+
+- either change the default management tag to your task branch
+- or pass your task branch as a parameter
--- scripts/workflow/action-infra-stack.sh
+++ scripts/workflow/action-infra-stack.sh
@@ -38,7 +38,6 @@ export TF_VAR_oidc_provider_url="https://token.actions.githubusercontent.com"
 export TF_VAR_oidc_thumbprint=$THUMBPRINT
 export TF_VAR_oidc_client="sts.amazonaws.com"
 
-echo Thumbprint "$TF_VAR_oidc_thumbprint"
 # check exports have been done
 EXPORTS_SET=0
 # Check key variables have been exported - see above
@@ -138,8 +137,6 @@ fi
 terraform-initialise
 #
 terraform workspace select "$WORKSPACE" || terraform workspace new "$WORKSPACE"
-#
-echo Pos init
 # plan
 if [ -n "$ACTION" ] && [ "$ACTION" = 'plan' ] ; then
   terraform plan \
