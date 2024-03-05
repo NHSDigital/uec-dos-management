f7d4279596bed9bc469f69b18e0a311986dba733
Jonathan Pearce <j.pearce1@nhs.net>
2024-03-05 14:28:04 +0000

    DR-5962 Refactor deployment to test env

--- scripts/workflow/push-tag.sh
+++ scripts/workflow/push-tag.sh
@@ -14,9 +14,27 @@ if [ -z "$TAG" ] ; then
     EXPORTS_SET=1
 fi
 
+if [ -z "$TAG_OVERWRITE" ] ; then
+    echo TAG_OVERWRITE not set
+    EXPORTS_SET=1
+fi
+
 if [ $EXPORTS_SET = 1 ] ; then
   echo One or more parameters not set
   exit 1
+fi
+
+echo "Checking to see if tag already exists: $TAG"
+if [ $(git tag -l "$TAG") ]; then
+    echo "Tag exists"
+    if [ "$TAG_OVERWRITE" == "yes" ]; then
+      git push --delete origin $TAG
+    else
+      echo "Exiting with error as we cannot overwrite an existing tag"
+      exit 1
+    fi
+else
+    echo "Tag does not exist"
 fi
 
 echo "About to push tag: $TAG"
