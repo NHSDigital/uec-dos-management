8335ffa1679ff5036139d7f311bc5cc41bec63fa
Jonathan Pearce <j.pearce1@nhs.net>
2024-03-05 10:01:43 +0000

    DR-5962 Refactor deployment to test env

--- scripts/workflow/tag-code.sh
+++ scripts/workflow/tag-code.sh
@@ -3,49 +3,33 @@
 # fail on first error
 set -e
 # This script generates a git tag
-# The format for the tag is $TAG_TYPE-$TERRAFORM_WORKSPACE_NAME-$COMMIT_HASH_SHORT
+# The format for the tag is $WORKSPACE-$COMMIT_HASH_SHORT-$TAG_TYPE
 # Where
-# TAG_TYPE defaults to T but can we set by exporting an alternative value before calling script
-# BRANCH_NAME =
-# COMMIT_HASH_SHORT = short version of the commit hash (ie commit to be tagged)
+# WORKSPACE is the workspace that we are generating the tag for. This really equates to the Jira task number
+# TAG_TYPE is the type of tag to generate. Usually this equates to the environment that we want to deploy the tagged artifacts into; i.e. test, stage, prod
 
-# functions
-source ./scripts/project-common.sh
-source ./scripts/functions/terraform-functions.sh
-source ./scripts/functions/git-functions.sh
-
-# check exports have been done
 EXPORTS_SET=0
 
+if [ -z "$WORKSPACE" ] ; then
+    echo WORKSPACE not set
+    EXPORTS_SET=1
+fi
+
 if [ -z "$TAG_TYPE" ] ; then
-    echo TAG_TYPE defaulting to test
-    TAG_TYPE="test"
-fi
-
-if [ -z "$BRANCH_NAME" ] ; then
-    echo BRANCH_NAME not set
-    EXPORTS_SET=1
-fi
-
-export_terraform_workspace_name
-export_short_commit_hash
-
-if [ -z "$TERRAFORM_WORKSPACE_NAME" ] ; then
-    echo TERRAFORM_WORKSPACE_NAME not derived
-    EXPORTS_SET=1
-fi
-if [ -z "$COMMIT_HASH_SHORT" ] ; then
-    echo COMMIT_HASH_SHORT not derived
+    echo TAG_TYPE not set
     EXPORTS_SET=1
 fi
 
 if [ $EXPORTS_SET = 1 ] ; then
-  echo One or more exports not set
+  echo One or more parameters not set
   exit 1
 fi
 
-# continue if required exports set
-generate_tag
+COMMIT_HASH_SHORT="$(git rev-parse --short HEAD)"
+TAG="$TERRAFORM_WORKSPACE_NAME-$COMMIT_HASH_SHORT-$TAG_TYPE"
 
+echo "Generated tag: $TAG"
+
+export TAG
 
 
