c3ad71b5db06c6c2c0cc9af8a7f8afe7a7f27c8c
Jonathan Pearce <j.pearce1@nhs.net>
2024-03-06 14:33:54 +0000

    DR-774 Bug fix on dervice workspace

--- scripts/workflow/derive-workspace.sh
+++ scripts/workflow/derive-workspace.sh
@@ -12,42 +12,29 @@ if [ "$TRIGGER" == "tag" ] ; then
   export WORKSPACE
   echo "Workspace from tag: $WORKSPACE"
 else
-  # If we are dealing with a push action and the trigger is not a tag, we'll need to look at the branch name
-  # to derive the workspace
-  if [ "$TRIGGER_ACTION" == "push" ] || [ "$TRIGGER_ACTION" == "pull_request" ] ; then
-    BRANCH_NAME="${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}"
-    BRANCH_NAME=$(echo $BRANCH_NAME | sed 's/refs\/heads\/task/task/g')
 
-    if [ "$BRANCH_NAME" == "main" ] ; then
-      WORKSPACE="default"
-      echo "Workspace from main: $WORKSPACE"
-    else
-      IFS='/' read -r -a name_array <<< "$BRANCH_NAME"
-      IFS='_' read -r -a ref <<< "${name_array[1]}"
-      WORKSPACE=$(echo "${ref[0]}" | tr "[:upper:]" "[:lower:]")
-      echo "Workspace every: $WORKSPACE"
-    fi
+  # If we are dealing with a push action or workflow_dispatch and the trigger is not a tag, we'll need to look at the branch name
+  # to derive the workspace
+  if [ "$TRIGGER_ACTION" == "push" ] || [ "$TRIGGER_ACTION" == "workflow_dispatch" ] ; then
+    BRANCH_NAME="${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}"
+    BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/refs\/heads\/task/task/g')
+  # If trigger action is pull_request we will need to derive the workspace from the triggering head reference
+  elif [ "$TRIGGER_ACTION" == "pull_request" ] ; then
+    BRANCH_NAME="$TRIGGER_HEAD_REFERENCE"
+    BRANCH_NAME=$(echo "$BRANCH_NAME" | sed 's/refs\/heads\/task/task/g')
   fi
 
-  # If trigger action is pull_request we will need to derive the workspace from the triggering head reference
-  if [ "$TRIGGER_ACTION" == "pull_request" ] ; then
-
-    BRANCH_NAME="$TRIGGER_HEAD_REFERENCE"
-    BRANCH_NAME=$(echo $BRANCH_NAME | sed 's/refs\/heads\/task/task/g')
-
-    if [ "$BRANCH_NAME" == "main" ] ; then
-      WORKSPACE="default"
-      echo "Workspace from main: $WORKSPACE"
-    else
-      IFS='/' read -r -a name_array <<< "$BRANCH_NAME"
-      IFS='_' read -r -a ref <<< "${name_array[1]}"
-      WORKSPACE=$(echo "${ref[0]}" | tr "[:upper:]" "[:lower:]")
-      echo "Workspace every: $WORKSPACE"
-    fi
+  if [ "$BRANCH_NAME" == "main" ] ; then
+    WORKSPACE="default"
+    echo "Workspace from main: $WORKSPACE"
+  else
+    IFS='/' read -r -a name_array <<< "$BRANCH_NAME"
+    IFS='_' read -r -a ref <<< "${name_array[1]}"
+    WORKSPACE=$(echo "${ref[0]}" | tr "[:upper:]" "[:lower:]")
+    echo "Workspace every: $WORKSPACE"
   fi
 
 fi
-
 
 # We will need some further logic here to derive the workspace upon branch deletion
 # TBC
