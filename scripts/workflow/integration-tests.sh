32fa8870ec645f75b9c13d349efaa88de65a39b9
Tim Rickwood <tim.rickwood@nhs.net>
2024-03-06 15:18:05 +0000

    DR-771 To include test step

--- scripts/workflow/integration-tests.sh
+++ scripts/workflow/integration-tests.sh
@@ -6,29 +6,35 @@ set -e
 #
 APPLICATION_TEST_DIR='tests/integration'
 
-source ./scripts/functions/git-functions.sh
+# source ./scripts/functions/git-functions.sh
 
 # identify the workspace name and set TERRAFORM_WORKSPACE_NAME
-export_terraform_workspace_name
+# export_terraform_workspace_name
+export APPLICATION_TEST_DIR="${APPLICATION_TEST_DIR:-"tests/integration"}"
 
 # check export has been done
 EXPORTS_SET=0
-if [ -z "$TERRAFORM_WORKSPACE_NAME" ] ; then
-  echo Set TERRAFORM_WORKSPACE_NAME
+if [ -z "$WORKSPACE" ] ; then
+  echo Set WORKSPACE to the workspace to action the terraform in
+  EXPORTS_SET=1
+fi
+
+if [ -z "$APPLICATION_TEST_DIR" ] ; then
+  echo Set APPLICATION_TEST_DIR to directory holding int test code
   EXPORTS_SET=1
 fi
 
 if [ $EXPORTS_SET = 1 ] ; then
-  echo TERRAFORM_WORKSPACE_NAME exports not set
+  echo One or more exports not set
   exit 1
 fi
 
 echo "Installing requirements"
-pip install -r $APPLICATION_TEST_DIR/requirements.txt
+pip install -r "$APPLICATION_TEST_DIR"/requirements.txt
 
 echo "Running integration tests"
-cd $APPLICATION_TEST_DIR
-behave --tags=pipeline_tests -D workspace=$TERRAFORM_WORKSPACE_NAME --format=allure -o allure-results;
+cd "$APPLICATION_TEST_DIR" || exit
+behave --tags=pipeline_tests -D workspace="$WORKSPACE" --format=allure -o allure-results;
 
-echo "next generating report"
+echo "Generating allure report"
 allure generate --single-file -c -o  allure-reports;
