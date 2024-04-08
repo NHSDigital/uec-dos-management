65ca948c2fef7bfbd0784839a4bccdb565d7321f
Tim Rickwood <tim.rickwood@nhs.net>
2024-04-08 17:14:55 +0100

    DR-780 Revert failed sonar bugfix

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('axe enbled');
@@ -26,4 +26,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
