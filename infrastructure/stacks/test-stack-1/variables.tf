1dcb416fbc2dd554dc9a77dff19554a7d4d9de57
Tim Rickwood <tim.rickwood@nhs.net>
2024-04-15 15:11:21 +0100

    DR-780 Separate ui test stack

--- infrastructure/stacks/test-stack-1/variables.tf
+++ infrastructure/stacks/test-stack-1/variables.tf
@@ -1,11 +1,3 @@
-variable "front-end-s3-bucket-name" {
-  description = "CM Front End"
-}
-
-variable "website_map" {
-  description = "Map of static website hosting"
-}
-
 variable "force_destroy" {
   description = "Whether to forcefully destroy the bucket when it contains objects"
   type        = bool
