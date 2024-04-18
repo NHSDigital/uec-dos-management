5419daf47a0e4009ed24ec13a80a81b731bc6607
Tim Rickwood <tim.rickwood@nhs.net>
2024-04-18 14:25:37 +0100

    DR-630 Tweak shell script

--- scripts/workflow/install-npm-dependencies.sh
+++ scripts/workflow/install-npm-dependencies.sh
@@ -3,7 +3,7 @@ FRONTEND_DIRECTORY=src/frontend
 # Navigate to the frontend directory if it exists
 if [ -d "$FRONTEND_DIRECTORY" ]; then
   cd "$FRONTEND_DIRECTORY"  || exit
-  # Install dependencies
+  # Install node dependencies
   npm ci
 else
   echo No frontend source code
