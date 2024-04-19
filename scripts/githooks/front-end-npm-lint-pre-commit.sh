ddebe49db7b2f56e496dc7947616926d3bd8dddb
Tim Rickwood <tim.rickwood@nhs.net>
2024-04-19 08:44:01 +0100

    DR-630 Test change to src backend

--- scripts/githooks/front-end-npm-lint-pre-commit.sh
+++ scripts/githooks/front-end-npm-lint-pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # FRONTEND_DIRECTORY=src/backend
-# FRONT_END_DIR='src/frontend'
-export FRONT_END_DIR="${FRONT_END_DIR:-"src/frontend"}"
+# FRONT_END_DIR='src/backend'
+export FRONT_END_DIR="${FRONT_END_DIR:-"src/backend"}"
 # Navigate to the frontend directory if it exists
 if [ -d "$FRONT_END_DIR" ]; then
   cd "$FRONT_END_DIR"  || exit
