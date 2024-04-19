14707c44cfdd67045c27f1d675cfc1c8978acb6b
Tim Rickwood <tim.rickwood@nhs.net>
2024-04-19 09:01:37 +0100

    DR-630 New npm lint action

---
+++ scripts/workflow/npm-lint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export FRONT_END_DIR="${FRONT_END_DIR:-"src/backend"}"
+# Navigate to the frontend directory if it exists
+if [ -d "$FRONT_END_DIR" ]; then
+  cd "$FRONT_END_DIR"  || exit
+  # Run lint command
+  npm run lint
+else
+  echo No frontend source code
+fi
