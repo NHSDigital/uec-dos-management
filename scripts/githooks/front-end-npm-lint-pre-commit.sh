defc8a3cb70e1fdcb59589136559652d665dbeb6
Tim Rickwood <tim.rickwood@nhs.net>
2024-04-19 08:08:41 +0100

    DR-630 Src frontend passed

--- scripts/githooks/front-end-npm-lint-pre-commit.sh
+++ scripts/githooks/front-end-npm-lint-pre-commit.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-FRONTEND_DIRECTORY=src/backend
+# FRONTEND_DIRECTORY=src/backend
+# FRONT_END_DIR='src/frontend'
+export FRONT_END_DIR="${FRONT_END_DIR:-"src/frontend"}"
 # Navigate to the frontend directory if it exists
 if [ -d "$FRONTEND_DIRECTORY" ]; then
   cd "$FRONTEND_DIRECTORY"  || exit
   # Run the linter #
   npm run lint
 else
-  echo No code to check
+  echo No code to lint
 fi
 
