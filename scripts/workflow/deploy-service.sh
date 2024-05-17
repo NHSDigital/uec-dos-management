d2cdbcadfb41b5603ced2a45298b1a47290d9108
Tim Rickwood <tim.rickwood@nhs.net>
2024-05-17 16:48:38 +0100

    DR-880 Wip working lambda code deployment

--- scripts/workflow/deploy-service.sh
+++ scripts/workflow/deploy-service.sh
@@ -4,23 +4,71 @@
 set -e
 
 export SERVICE="${SERVICE:-""}"
-export TAG="${TAG:-""}"
+export COMMIT_HASH="${COMMIT_HASH:-""}"
 export WORKSPACE="${WORKSPACE:-""}"
+export REPO_NAME="${REPO_NAME:-""}"
+export ENVIRONMENT="${ENVIRONMENT:-""}"
+export APPLICATION_ROOT_DIR="${APPLICATION_ROOT_DIR:-"application"}"
+# check exports have been done
+EXPORTS_SET=0
+# Check key variables have been exported - see above
+if [ -z "$SERVICE" ] ; then
+  echo Set SERVICE to name of service aka function to be deployed
+  EXPORTS_SET=1
+fi
 
-echo "Deploying service: ${SERVICE} version: ${TAG} into workspace: ${WORKSPACE}"
+if [ -z "$COMMIT_HASH" ] ; then
+  echo Set COMMIT_HASH to identify which artefacts to be deployed
+  EXPORTS_SET=1
+fi
 
+if [ -z "$WORKSPACE" ] ; then
+  echo Set WORKSPACE to the workspace to action the terraform in
+  EXPORTS_SET=1
+fi
+
+if [ -z "$REPO_NAME" ] ; then
+  echo Set REPOSITORY to the domain repository for the service to be deployed
+  EXPORTS_SET=1
+fi
+
+if [ -z "$ENVIRONMENT" ] ; then
+  echo Set ENVIRONMENT to required environment eg dev, test, staging etc
+  EXPORTS_SET=1
+fi
+
+if [ -z "$APPLICATION_ROOT_DIR" ] ; then
+  echo Set APPLICATION_ROOT_DIR to root directory of the service being deployed - eg application
+  EXPORTS_SET=1
+fi
+
+if [ $EXPORTS_SET = 1 ] ; then
+  echo One or more exports not set
+  exit 1
+fi
+ARTEFACT_BUCKET_NAME="nhse-mgmt-${REPO_NAME}-artefacts"
+RELEASED_ARTEFACT_BUCKET_NAME="${ARTEFACT_BUCKET_NAME}-released"
 LAMBDA_FUNCTION="${SERVICE}"
+DEPLOYMENT_FILE_NAME="$SERVICE.zip"
 
 if [ "${WORKSPACE}" != "default" ]; then
   LAMBDA_FUNCTION="${SERVICE}-${WORKSPACE}"
 fi
+echo "Pulling deployment artefact ${DEPLOYMENT_FILE_NAME} for service ${SERVICE}"
+echo "From ${ARTEFACT_BUCKET_NAME}/${WORKSPACE}/${COMMIT_HASH}"
+echo "For deployment to the lambda ${LAMBDA_FUNCTION} in the ${WORKSPACE} workspace in the ${ENVIRONMENT} environment"
 
-echo "Deploying service into Lambda function: ${LAMBDA_FUNCTION}"
+# TODO can i pass file directly as zip-file parameter and avoid landing it in directory
+cd ./"${APPLICATION_ROOT_DIR}"/"${SERVICE}"
+aws s3api get-object --bucket "${ARTEFACT_BUCKET_NAME}" --key "${WORKSPACE}/${COMMIT_HASH}/${DEPLOYMENT_FILE_NAME}" "${DEPLOYMENT_FILE_NAME}"
 
-cd ./"${DIRECTORY}"/"${SERVICE}"
-
-LAMBDA_OUTPUT=$(aws lambda update-function-code --function-name="${LAMBDA_FUNCTION}" --zip-file=fileb://deployment.zip --publish)
+LAMBDA_OUTPUT=$(aws lambda update-function-code --function-name="${LAMBDA_FUNCTION}" --zip-file=fileb://"${DEPLOYMENT_FILE_NAME}" --publish)
 LATEST_VERSION=$(jq -r '.Version' --compact-output <<< "$LAMBDA_OUTPUT" )
 PREVIOUS_VERSION=$(expr "${LATEST_VERSION}" - 1)
-echo "Latest version: ${LATEST_VERSION}"
-echo "Previous version: ${PREVIOUS_VERSION}"
+
+echo "Artefact ${ARTEFACT_BUCKET_NAME}/${WORKSPACE}/${COMMIT_HASH}${DEPLOYMENT_FILE_NAME}"
+echo "Deployed to version ${LATEST_VERSION} of the lambda ${LAMBDA_FUNCTION} in the ${WORKSPACE} in the ${ENVIRONMENT} environment"
+echo "Replacing previous version: ${PREVIOUS_VERSION}"
+
+# TODO
+# add tag to show deployed to $ENVIRONMENT
