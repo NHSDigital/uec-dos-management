411a688ab2bcbe87f5d2ef8fe3d49b22ad1de3c3
Tim Rickwood <tim.rickwood@nhs.net>
2024-05-24 10:04:39 +0100

    DR-880 Revert use of new actions

--- scripts/workflow/derive-hash-from-sha.sh
+++ scripts/workflow/derive-hash-from-sha.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 export SHA="${SHA:-""}"
+echo Not used
+# COMMIT_HASH="Unknown"
+# # check exports have been done
+# EXPORTS_SET=0
+# # Check key variables have been exported - see above
+# if [ -z "$SHA" ] ; then
+#   echo Set SHA to required sha for commit on required branch
+#   EXPORTS_SET=1
+# fi
 
-COMMIT_HASH="Unknown"
-# check exports have been done
-EXPORTS_SET=0
-# Check key variables have been exported - see above
-if [ -z "$SHA" ] ; then
-  echo Set SHA to required sha for commit on required branch
-  EXPORTS_SET=1
-fi
+# if [ $EXPORTS_SET = 1 ] ; then
+#   echo One or more exports not set
+#   exit 1
+# fi
 
-if [ $EXPORTS_SET = 1 ] ; then
-  echo One or more exports not set
-  exit 1
-fi
+# COMMIT_HASH=$(git rev-parse --short "$SHA")
 
-COMMIT_HASH=$(git rev-parse --short "$SHA")
-
-export COMMIT_HASH
+# export COMMIT_HASH
