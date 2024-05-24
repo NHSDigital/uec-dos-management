de9c79523c7b4c1a109a1a906f61ae984797181e
Tim Rickwood <tim.rickwood@nhs.net>
2024-05-24 09:09:06 +0100

    DR-880 Separate hash used by prs

--- scripts/workflow/derive-hash-from-sha.sh
+++ scripts/workflow/derive-hash-from-sha.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export SHA="${SHA:-""}"
-export PULL_REQUEST="${PULL_REQUEST:-""}"
 
+COMMIT_HASH="Unknown"
 # check exports have been done
 EXPORTS_SET=0
 # Check key variables have been exported - see above
@@ -15,11 +15,6 @@ if [ $EXPORTS_SET = 1 ] ; then
   exit 1
 fi
 
-commit_hash=$(git rev-parse --short "$SHA")
-if [ -z "$PULL_REQUEST" ] ; then
-    echo "setting commit_hash: $commit_hash"
-    echo "commit_hash=$commit_hash" >> $GITHUB_OUTPUT
-else
-    echo "Setting artefact_commit_hash: $commit_hash"
-    echo "artefact_commit_hash=$commit_hash" >> $GITHUB_OUTPUT
-fi
+COMMIT_HASH=$(git rev-parse --short "$SHA")
+
+export COMMIT_HASH
