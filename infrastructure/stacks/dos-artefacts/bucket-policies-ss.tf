c057b0c3b1968446a8455ee10712bf1a04c4b1b3
piravinth <piravinth.maheswaran1@nhs.net>
2024-07-08 12:02:13 +0100

    DR-930 Amend artefacts bucket policies

--- infrastructure/stacks/dos-artefacts/bucket-policies-ss.tf
+++ infrastructure/stacks/dos-artefacts/bucket-policies-ss.tf
@@ -8,6 +8,7 @@ data "aws_iam_policy_document" "bucket_policy_ss_artefacts" {
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/aws-reserved/sso.amazonaws.com/${var.aws_region}/AWSReservedSSO_DOS-Developer_8bdf3f98a2591a2b",
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_dev.value}:role/uec-dos-service-search-github-runner",
         "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_test.value}:role/uec-dos-service-search-github-runner",
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_int.value}:role/uec-dos-management-github-runner",
       ]
     }
     actions = [
@@ -33,6 +34,22 @@ data "aws_iam_policy_document" "bucket_policy_ss_artefacts" {
       "s3:DeleteObject",
       "s3:PutObject",
       "s3:PutObjectTagging"
+    ]
+    resources = [
+      "${module.ss_artefacts_bucket.s3_bucket_arn}/*"
+    ]
+  }
+
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_ssm_parameter.aws_account_id_int.value}:role/uec-dos-management-github-runner",
+      ]
+    }
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectTagging",
     ]
     resources = [
       "${module.ss_artefacts_bucket.s3_bucket_arn}/*"
