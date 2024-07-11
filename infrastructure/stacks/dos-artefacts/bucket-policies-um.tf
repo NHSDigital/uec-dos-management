74863354686040a4b206caf75061777a32f0ef0d
Tim Rickwood <tim.rickwood@nhs.net>
2024-07-11 08:54:31 +0100

    DR-930 Add tagging permission to bucket pol

--- infrastructure/stacks/dos-artefacts/bucket-policies-um.tf
+++ infrastructure/stacks/dos-artefacts/bucket-policies-um.tf
@@ -50,6 +50,7 @@ data "aws_iam_policy_document" "bucket_policy_um_artefacts" {
     actions = [
       "s3:GetObject",
       "s3:GetObjectTagging",
+      "s3:PutObjectTagging",
     ]
     resources = [
       "${module.um_artefacts_bucket.s3_bucket_arn}/*"
