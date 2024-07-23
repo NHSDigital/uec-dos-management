ffaf230bb9376a8354553fb9818b0152f5d9cf08
Solomon Omowuni <solomon.omowuni1@nhs.net>
2024-07-23 14:44:16 +0100

    DR-985 Deploy parameter store

---
+++ infrastructure/stacks/parameter_store/ssm-parameters.tf
@@ -0,0 +1,41 @@
+resource "aws_ssm_parameter" "dos_aws_account_id_dev" {
+  name        = "/dos/aws_account_id_dev"
+  description = "Id of current dev account"
+  type        = "SecureString"
+  tier        = "standard"
+}
+
+resource "aws_ssm_parameter" "dos_aws_account_id_test" {
+  name        = "/dos/aws_account_id_test"
+  description = "Id of current test account"
+  type        = "SecureString"
+  tier        = "Standard"
+}
+
+resource "aws_ssm_parameter" "dos_aws_account_id_int" {
+  name        = "/dos/aws_account_id_int"
+  description = "Id of current integration test account"
+  type        = "SecureString"
+  tier        = "Standard"
+}
+
+resource "aws_ssm_parameter" "dos_aws_account_id_preprod" {
+  name        = "/dos/aws_account_id_preprod"
+  description = ""
+  type        = "SecureString"
+  tier        = "Standard"
+}
+
+resource "aws_ssm_parameter" "dos_aws_account_id_prod" {
+  name        = "/dos/aws_account_id_prod"
+  description = "Id of current preprod account"
+  type        = "SecureString"
+  tier        = "Standard"
+}
+
+resource "aws_ssm_parameter" "dos_aws_account_id_prototype" {
+  name        = "/dos/aws_account_id_prototype"
+  description = "Id of the account used for prototype currently cm dev"
+  type        = "SecureString"
+  tier        = "Standard"
+}
