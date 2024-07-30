964831cb771d1e23348187f61f5fc541d8300a6e
Solomon Omowuni <solomon.omowuni1@nhs.net>
2024-07-30 13:33:34 +0100

    DR-985 Update param name

--- infrastructure/stacks/parameter_store/ssm-parameters.tf
+++ infrastructure/stacks/parameter_store/ssm-parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "dos_aws_account_id_dev" {
-  name        = "/dos/aws_account_id_dev_testing_changed"
+  name        = "/dos/aws_account_id_dev"
   description = "Id of current dev account"
   type        = "SecureString"
   tier        = "Standard"
@@ -13,7 +13,7 @@ resource "aws_ssm_parameter" "dos_aws_account_id_dev" {
 }
 
 resource "aws_ssm_parameter" "dos_aws_account_id_test" {
-  name        = "/dos/aws_account_id_test_testing"
+  name        = "/dos/aws_account_id_test"
   description = "Id of current test account"
   type        = "SecureString"
   tier        = "Standard"
@@ -27,7 +27,7 @@ resource "aws_ssm_parameter" "dos_aws_account_id_test" {
 }
 
 resource "aws_ssm_parameter" "dos_aws_account_id_int" {
-  name        = "/dos/aws_account_id_int_testing"
+  name        = "/dos/aws_account_id_int"
   description = "Id of current integration test account"
   type        = "SecureString"
   tier        = "Standard"
@@ -41,7 +41,7 @@ resource "aws_ssm_parameter" "dos_aws_account_id_int" {
 }
 
 resource "aws_ssm_parameter" "dos_aws_account_id_preprod" {
-  name        = "/dos/aws_account_id_preprod_testing"
+  name        = "/dos/aws_account_id_preprod"
   description = ""
   type        = "SecureString"
   tier        = "Standard"
@@ -55,7 +55,7 @@ resource "aws_ssm_parameter" "dos_aws_account_id_preprod" {
 }
 
 resource "aws_ssm_parameter" "dos_aws_account_id_prod" {
-  name        = "/dos/aws_account_id_prod_testing"
+  name        = "/dos/aws_account_id_prod"
   description = "Id of current preprod account"
   type        = "SecureString"
   tier        = "Standard"
@@ -69,7 +69,7 @@ resource "aws_ssm_parameter" "dos_aws_account_id_prod" {
 }
 
 resource "aws_ssm_parameter" "dos_aws_account_id_prototype" {
-  name        = "/dos/aws_account_id_prototype_testing"
+  name        = "/dos/aws_account_id_prototype"
   description = "Id of the account used for prototype currently cm dev"
   type        = "SecureString"
   tier        = "Standard"
