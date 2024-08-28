4e6ba4dd7b1e9c3a89cd3aec020dd0d38d8ddb3f
Tim Rickwood <tim.rickwood@nhs.net>
2024-08-28 10:24:39 +0100

    DR-1079 Remove dupl tags

--- infrastructure/stacks/route-53/acm.tf
+++ infrastructure/stacks/route-53/acm.tf
@@ -4,10 +4,6 @@ module "acm_certificate" {
   zone_id     = module.cm_route53.zone_id
   san_list    = ["*.uec-capacity-management.nhs.uk"]
 
-  tags = {
-    Environment = "production"
-    Owner       = "dos-rewrite"
-  }
 }
 
 output "certificate_arn" {
