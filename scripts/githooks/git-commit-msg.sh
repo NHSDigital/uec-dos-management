475580b1b45ddc764a4d826e007af2820f471cd8
timrickwood <48654585+timrickwood@users.noreply.github.com>
2024-11-14 15:45:26 +0000

    Change branch naming pattern

--- scripts/githooks/git-commit-msg.sh
+++ scripts/githooks/git-commit-msg.sh
@@ -3,8 +3,9 @@ source ./scripts/functions/git-functions.sh
 
 exit_code=0
 BUILD_COMMIT_MESSAGE=${BUILD_COMMIT_MESSAGE:-"$(cat $1)"}
+BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
 
-check_git_commit_message "$BUILD_COMMIT_MESSAGE"
+check_git_commit_message "$BUILD_COMMIT_MESSAGE" "$BRANCH_NAME"
 exit_code=$?
 
 exit $exit_code
