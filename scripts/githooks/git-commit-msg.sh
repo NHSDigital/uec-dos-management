f6aa7a9c7e0abf12ab680c0f5a14b25f14da69cb
timrickwood <48654585+timrickwood@users.noreply.github.com>
2024-11-15 14:50:03 +0000

    DR-1211 Add jira ref if missing

--- scripts/githooks/git-commit-msg.sh
+++ scripts/githooks/git-commit-msg.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 source ./scripts/functions/git-functions.sh
 
-exit_code=0
+# exit_code=0
+echo $1
 BUILD_COMMIT_MESSAGE=${BUILD_COMMIT_MESSAGE:-"$(cat $1)"}
 BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
 
 check_git_commit_message "$BUILD_COMMIT_MESSAGE" "$BRANCH_NAME"
-exit_code=$?
+# exit_code=$?
 
-exit $exit_code
+# TODO
+# exit $exit_code
+exit 1
